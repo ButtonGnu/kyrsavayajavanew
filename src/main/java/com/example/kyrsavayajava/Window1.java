08f3d8b3ff4edfb29b8c4408ba1041248098734a
Your Name <kirby.eight.00@bk.ru>
2022-06-22 02:31:08 +0300

    новая заявка

--- src/main/java/com/example/kyrsavayajava/Window1.java
+++ src/main/java/com/example/kyrsavayajava/Window1.java
@@ -1,12 +1,29 @@
 package com.example.kyrsavayajava;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 
 public class Window1 {
+    @FXML
+    public TextField name = new TextField();
+    @FXML
+    public TextField phone = new TextField();
+    @FXML
+    public TextField plateNr = new TextField();
+    @FXML
+    public TextField reason = new TextField();
+
     public void Open5(ActionEvent event){
 
         try{
@@ -35,5 +52,30 @@ public class Window1 {
 
     }
 
+    private String generateRequestReason() {
+        DamageGenerator dg              = new DamageGenerator();
+        Damage       damage          = dg.generateDamage(new JsonReader().readJson("поломки.json"));
+        List<String> damageDBStrings = new ArrayList<>();
+        for (int i = 0; i < damage.getSubCategories().size(); i++) {
+            DamageSubCategory damageSubCategory = damage.getSubCategories().get(i);
+            if (damageSubCategory.getSubType() != null) {
+                for (int j = 0; j < damageSubCategory.getSubType().size(); j++) {
+                    String   subCategoryType = damageSubCategory.getSubType().get(j);
+                    DamageDB e               = new DamageDB(damage.getCategory(), damageSubCategory.getType(), subCategoryType);
+                    damageDBStrings.add(e.toString());
+                }
+            } else {
+                DamageDB e = new DamageDB(damage.getCategory(), damageSubCategory.getType(), "");
+                damageDBStrings.add(e.toString());
+            }
+        }
+        return String.join("/", damageDBStrings);
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        String requestReason = generateRequestReason();
+        this.reason.setText(requestReason);
+    }
 }
 
